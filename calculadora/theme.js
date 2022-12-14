5f929aca3f4320f432cb028c284ed53a99172ce6
andres0818 <37464506+andres0818@users.noreply.github.com>
2022-12-13 19:34:43 -0500

    montaje calculadora y cuenta regresiva

---
+++ calculadora/theme.js
@@ -0,0 +1,28 @@
+const themePurple = () => {
+    document.querySelector("body").style.backgroundColor ="rgba(137, 43, 226, 0.726)";
+      document.querySelector(".container").style.backgroundColor = " rgba(0, 0, 0, 0.423)";
+
+  
+};
+const themeBlack = () => {
+    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector(".container").style.backgroundColor = "rgba(255, 255, 255, 0.361)";
+  
+};
+
+const themeWhite = () => {
+  
+    document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector(".container").style.backgroundColor = " rgba(0, 0, 0, 0.423)";
+
+  
+};
+document.onkeydown=()=>{
+    alert('no me robe el codigo -_-')
+return false
+}
+
+document.oncontextmenu=()=>{
+    alert('no me robe el codigo -_-')
+return false
+}
